11f668b8eddd5ee3f4563dab3f5eff16d49a47e9
nbilbo <ddtankx68@gmail.com>
2023-10-30 14:08:45 -0300

    refactor export_adults_to_excel

--- app/utils/excel.py
+++ app/utils/excel.py
@@ -64,14 +64,14 @@ def export_adults_to_excel(values: Dict[str, List[AdultEntity]], file_path: str)
     """
     with pd.ExcelWriter(file_path) as writer:
         for activity, registers in values.items():
-            pd.DataFrame(
-                {
-                    'Nome': [register.adult_name for register in registers],
-                    'Gênero': [register.adult_gender for register in registers],
-                    'CPF': [register.adult_cpf for register in registers],
-                    'RG': [register.adult_rg for register in registers],
-                    'Nascimento': [str_to_date(register.adult_birthdate) for register in registers],
-                    'Endereço': ['\n'.join(register.adult_address) for register in registers],
-                    'Contatos': ['\n'.join(register.adult_contacts) for register in registers],
-                }
-            ).to_excel(writer, sheet_name=activity, index=False)
+            data = {
+                'Nome': [register.adult_name for register in registers],
+                'Gênero': [register.adult_gender for register in registers],
+                'CPF': [register.adult_cpf for register in registers],
+                'RG': [register.adult_rg for register in registers],
+                'Nascimento': [str_to_date(register.adult_birthdate) for register in registers],
+                'Endereço': ['\n'.join(register.adult_address) for register in registers],
+                'Contatos': ['\n'.join(register.adult_contacts) for register in registers],
+            }
+            data_frame = pd.DataFrame(data)
+            data_frame.to_excel(writer, sheet_name=activity, index=False)
